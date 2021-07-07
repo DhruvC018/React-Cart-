bc2c815711b77b199442319876f8497d2512ec45
DhruvC018 <59206148+DhruvC018@users.noreply.github.com>
2021-07-07 15:25:15 +0530

    Added changes to the cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,6 +11,11 @@ class CartItem extends React.Component {   //CartItem will inherit some items fr
         } //we use this data in the jsx component. example in line 21.
     }
 
+    increaseQuantity =  () => {
+        //the arrow function binds the elements naturally. we can even use the bind keyword.
+        console.log('this.state', this.state);
+    }
+
     render (){  //for class component to be a react component we give the method render.
         const { price, title, qty } = this.state; //using object destructuring. getting the object, the one above created. We get the properties from the object
         return(
@@ -25,9 +30,23 @@ class CartItem extends React.Component {   //CartItem will inherit some items fr
                     <div style={ {color: '#777'} }> Qty: {qty} </div>
 
                     <div className="cart-item-actions">
-                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" />
-                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
-                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png" />
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/992/992651.png" 
+                            onClick={this.increaseQuantity} //Event listener addded here. Here we can use the bind function in order to use it better. A better alternative to bind function is using the arrow function.
+                            //onClick = {this.increaseQuantity.bind(this) // here we bind with the object.} this is how binding takes place. to improve this we use the arrow function.
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/992/992683.png" 
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/1214/1214428.png" 
+                        />
                         {/* Buttons */}
                     </div>
                 </div>
