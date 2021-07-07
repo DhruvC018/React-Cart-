3301d34547ce05999c14d90ba6e407f4b5db4c53
DhruvC018 <59206148+DhruvC018@users.noreply.github.com>
2021-07-07 13:27:31 +0530

    Added changes to the cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class CartItem extends React.Component {   //CartItem will inherit some items from the class Component in the react module.Hence this is a class based component.
+    constructor() {  //This is used to add state. A state is a plain javascript object, which is used to store the data for the current component.
+        super(); //calling this calls the constructor of the component class.
+        this.state = { //initially this will throw an error, saying super constructor must be called. It is an inhertance error.
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        } //we use this data in the jsx component. example in line 21.
+    }
+
     render (){  //for class component to be a react component we give the method render.
+        const { price, title, qty } = this.state; //using object destructuring. getting the object, the one above created. We get the properties from the object
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,11 +20,14 @@ class CartItem extends React.Component {   //CartItem will inherit some items fr
                 </div>
                 <div className="right-block">
     
-                    <div style={ { fontSize: 25 } }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs 999</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
+                    <div style={ { fontSize: 25 } }> {title} </div> 
+                    <div style={ {color: '#777'} }> Rs {price} </div>
+                    <div style={ {color: '#777'} }> Qty: {qty} </div>
 
                     <div className="cart-item-actions">
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png" />
                         {/* Buttons */}
                     </div>
                 </div>
