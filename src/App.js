0e0d25fdbc36bf211fee189d54933a6b63080a08
DhruvC018 <59206148+DhruvC018@users.noreply.github.com>
2021-08-16 21:58:08 +0530

    cart app completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CartItem from './CartItem';
 import Cart from './Cart'
 import Navbar from './Navbar';
+import firebase from 'firebase';
+import 'firebase/firestore'; 
 
 class App extends React.Component { //here, App is a function based component.
   
@@ -9,40 +11,111 @@ class App extends React.Component { //here, App is a function based component.
     super(); 
       this.state = { 
         products: [ //array created and here we will map over hte items of the array and put it in our list.
-          {
-            price: 99,
-            title: 'Watch',
-            qty: 10,
-            img: 'https://www.longines.com/media/catalog/product/cache/8db0cbef53b094d1dd99f8463500f53a/t/h/the-longines-master-collection-l2-673-4-78-3-detailed-view-2000x2000-1.jpg',
-            id: 1
-          },
-          {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: 'https://images-na.ssl-images-amazon.com/images/I/71MHTD3uL4L._SL1500_.jpg',
-            id: 2
-          },
-          {
-            price: 999,
-            title: 'Laptop',
-            qty: 4,
-            img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202011?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1613672894000',
-            id: 3
-          }
-        ]
+
+          ///// commenting this out because now we will be fetching our data from the firebase /////
+          // {
+          //   price: 99,
+          //   title: 'Watch',
+          //   qty: 10,
+          //   img: 'https://www.longines.com/media/catalog/product/cache/8db0cbef53b094d1dd99f8463500f53a/t/h/the-longines-master-collection-l2-673-4-78-3-detailed-view-2000x2000-1.jpg',
+          //   id: 1
+          // },
+          // {
+          //   price: 999,
+          //   title: 'Mobile Phone',
+          //   qty: 1,
+          //   img: 'https://images-na.ssl-images-amazon.com/images/I/71MHTD3uL4L._SL1500_.jpg',
+          //   id: 2
+          // },
+          // {
+          //   price: 999,
+          //   title: 'Laptop',
+          //   qty: 4,
+          //   img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202011?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1613672894000',
+          //   id: 3
+          // }
+        ],
+        loading: true
       }
+      this.db = firebase.firestore();
+  }
+
+  // life cycle method
+  componentDidMount () {
+    // firebase 
+    //   .firestore()
+    //   .collection('products')
+    //   .get() //returns a promise and a query snapshot
+    //   .then((snapshot) => { //snapshot is passed as an argument can be ranamed to anything. it will give us a snapshot of the database at that time.
+    //     console.log(snapshot);
+    //     snapshot.docs.map((doc) => {
+    //       console.log(doc.data())
+    //     });
+    //     const products = snapshot.docs.map((doc) => {
+    //       const data = doc.data();
+
+    //       data['id'] = doc.id //this is because the document has the id and not the ibject dispalyed when we console.log
+    //       return data; //we return doc.data and not doc because we need the data inside the doc and not the doc itself.
+    //     })
+
+    //     this.setState({
+    //       products,
+    //       loading: false
+    //     })
+    //   })
+
+    // in order to update it automatically and we don't have to refresh it
+
+    this.db
+      .collection('products')
+      // .get() //returns a promise and a query snapshot
+      .where('price', '==', 999) //return all products wiht price 99. SIilarly we can look for elements with various values 
+      // .orderBy('price') //gives uis the price in ascending order
+      .orderBy('price', 'desc') //gives us the price in decending order
+      .onSnapshot((snapshot) => { //snapshot is passed as an argument can be ranamed to anything. it will give us a snapshot of the database at that time.
+        //onSnapshot is an inbuilt keyword which is provided to us by react.
+        console.log(snapshot);
+        snapshot.docs.map((doc) => {
+          console.log(doc.data())
+        });
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+
+          data['id'] = doc.id //this is because the document has the id and not the ibject dispalyed when we console.log
+          return data; //we return doc.data and not doc because we need the data inside the doc and not the doc itself.
+        })
+
+        this.setState({
+          products,
+          loading: false
+        })
+      })
   }
     
     handleIncreaseQty = (product) => {
         const{ products } = this.state;
         const index = products.indexOf(product);
 
-        products[index].qty += 1;
+        // products[index].qty += 1; //instead of doing this, we will make the changes in the firebase.
 
-        this.setState({
-            products: products
-        })
+        // this.setState({
+        //     products: products
+        // });
+
+        ///// Now we will make changes directy in firebase.
+
+        const docRef = this.db.collection('products').doc(products[index].id);
+
+        docRef
+          .update({
+            qty: products[index].qty+1
+          })
+          .then(() => {
+            console.log('Document Updated successfully')
+          })
+          .catch((error) => {
+            console.log('Error is: ', error);
+          })
     }
 
     handleDecreaseQty = (product) => {
@@ -53,22 +126,43 @@ class App extends React.Component { //here, App is a function based component.
             return;
         }
 
-        products[index].qty -= 1;
+        // products[index].qty -= 1;
 
-        this.setState({
-            products: products
-        })
+        // this.setState({
+        //     products: products
+        // })
+
+        const docRef = this.db.collection('products').doc(products[index].id);
+
+        docRef
+          .update({
+            qty: products[index].qty-1
+          })
+          .then(() => {
+            console.log('Document Updated successfully')
+          })
+          .catch((error) => {
+            console.log('Error is; ', error);
+          })
     }
 
     handleDeleteProduct = (id) => {
         const{products} = this.state;
 
-        const items = products.filter((item) => item.id!=id);
+        // const items = products.filter((item) => item.id!=id);
     
-        this.setState({
-            products: items 
-        })
-    }
+        // this.setState({
+        //     products: items 
+        // })
+
+        const docRef = this.db.collection('products').doc(id); //we simplly pass id and not products[index].id because the id has already been passed as a parameter.
+        
+        docRef
+          .delete()
+          .then(() => {
+            console.log('Deleted successfully')
+          })
+      }
 
     getCartCount = () => {
        const { products } = this.state;
@@ -86,23 +180,44 @@ class App extends React.Component { //here, App is a function based component.
 
       let cartTotal = 0;
       products.map((product) => {
-        cartTotal = cartTotal + product.qty * product.price;
-      })
+        if(product.qty > 0) {
+          cartTotal = cartTotal + product.qty * product.price;
+        }
+        return '';
+      });
       return cartTotal;
     }
 
+    addProduct = () => {
+      this.db
+        .collection('products')
+        .add({
+          img: '',
+          price: 900,
+          qty: 3,
+          title: 'washing machine'
+        })
+        .then((docRef) => {  //docRef is the reference of the object in the firebase
+          console.log('Product has been added', docRef) //the above will be converted into a document and then will be added to our collection products.
+        })
+        .catch((error) => {    //catch used for catching errors.
+          console.log('Erro: ', error);
+        })  
+    }
+
     render() {
-      const { products } = this.state;
+      const { products, loading } = this.state;
     return (
       <div className="App">
         <Navbar count = {this.getCartCount()} />
+        {/* <button onClick = {this.addProduct} style = {{padding: 20, fontSize: 20}}>Add a product</button> */}
         <Cart
           products = {products}
           onIncreaseQty={this.handleIncreaseQty}
           onDecreaseQty={this.handleDecreaseQty}
           onDeleteProduct={this.handleDeleteProduct}
         />
-        
+        {loading && <h3>Loading Products...</h3>}
         <div style={{fontSize: 20, padding: 10}}>TOTAL: {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/firestore'; 
+
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZgLTpBkHga8KC5Hla9kwQMbRuOOOR96E",
+  authDomain: "cart-2d939.firebaseapp.com",
+  projectId: "cart-2d939",
+  storageBucket: "cart-2d939.appspot.com",
+  messagingSenderId: "339989241245",
+  appId: "1:339989241245:web:863e96a682a0cffe33e374"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +25,4 @@ ReactDOM.render(
 );
 
 
+//Add firebase config here.
